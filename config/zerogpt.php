4f10d79c5699a40759823424293ed940974df06b
mikeyperes <michael@mike-ro-tech.com>
2026-04-09 03:49:18 -0400

    Version bump

--- config/zerogpt.php
+++ config/zerogpt.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'enabled' => true,
     'api_url' => 'https://api.zerogpt.com/api/detect/detectText',
     'setting_keys' => [
