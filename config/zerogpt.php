4b2e5fd6cb848c97fee250529cd996d22319ab41
Michael Peres <mikeyperes@users.noreply.github.com>
2026-04-20 00:52:29 +0000

    Register 3-level sidebar hierarchy: domain + section mapping v2.0.3

--- config/zerogpt.php
+++ config/zerogpt.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '2.0.2',
+    'version' => '2.0.3',
     'enabled' => true,
     'api_url' => 'https://api.zerogpt.com/api/detect/detectText',
     'setting_keys' => [
--- src/Providers/ZeroGptServiceProvider.php
+++ src/Providers/ZeroGptServiceProvider.php
@@ -31,6 +31,11 @@ class ZeroGptServiceProvider extends ServiceProvider
         // Sidebar link
         if (class_exists(\hexa_core\Services\PackageRegistryService::class)) {
             $registry = app(\hexa_core\Services\PackageRegistryService::class);
+            // HWS-SIDEBAR-MENU-3L-BEGIN
+            $registry->registerDomainGroup('Discovery', 'M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z', 20);
+            $registry->registerSectionGroup('Content', 'Discovery', '', 20);
+            // HWS-SIDEBAR-MENU-3L-END
+
             $registry->registerSidebarLink('zerogpt.settings', 'ZeroGPT', 'M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z', 'Content', 'zerogpt', 80);
         }
     }
