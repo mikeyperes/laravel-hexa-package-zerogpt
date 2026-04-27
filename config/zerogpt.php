a1d084848548e5de1688a5497050bc40db262ed4
mikeyperes <mikeyperes@users.noreply.github.com>
2026-04-27 03:29:43 +0000

    feat(zerogpt): register package metadata

--- config/zerogpt.php
+++ config/zerogpt.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '2.0.3',
+    'version' => '2.0.4',
     'enabled' => true,
     'api_url' => 'https://api.zerogpt.com/api/detect/detectText',
     'setting_keys' => [
